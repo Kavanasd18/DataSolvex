f923d8ab656b1175432fdb9c2f41c7bb51c70c64
Kavanasd18 <kavana.sd@datasturdy.com>
2025-11-28 10:03:10 +0530

    Changed replication dashboard frontend

--- login_AD_unified/app.py
+++ login_AD_unified/app.py
@@ -3969,6 +3969,6 @@ def dashboard_summary():
 # Run the Flask application
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=9099,debug='True')
+    app.run(host='0.0.0.0', port=8099,debug='True')
 
 #app.run(host='0.0.0.0', port=5000)
